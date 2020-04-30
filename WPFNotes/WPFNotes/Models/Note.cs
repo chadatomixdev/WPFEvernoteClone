fd5ec148d9d4886cf532adb8f5ccaeb35caa2726
Chad Bonthuys <chad@atomixdev.com>
2020-04-30 12:45:02 +0100

    Adding Models and INotifyPropertyChanged Interface

---
+++ WPFNotes/WPFNotes/Models/Note.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNotes.Models
+{
+    public class Note : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnProperyChanged("Id");
+            }
+        }
+
+        private int notebookId;
+
+        public int NotebookId
+        {
+            get { return notebookId; }
+            set
+            {
+                notebookId = value;
+                OnProperyChanged("NotebookId");
+            }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnProperyChanged("Title");
+            }
+        }
+
+        private DateTime createdTime;
+
+        public DateTime CreatedTime
+        {
+            get { return createdTime; }
+            set
+            {
+                createdTime = value;
+                OnProperyChanged("CreatedTime");
+            }
+        }
+
+        private DateTime updatedTime;
+
+        public DateTime UpdatedTime
+        {
+            get { return createdTime; }
+            set
+            {
+                createdTime = value;
+                OnProperyChanged("UpdatedTime");
+            }
+        }
+
+        private string filelocation;
+
+        public string FileLocation
+        {
+            get { return filelocation; }
+            set
+            {
+                filelocation = value;
+                OnProperyChanged("FileLocation");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnProperyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPFNotes/WPFNotes/Models/User.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNotes.Models
+{
+    public class User : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string lastname;
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                lastname = value;
+                OnPropertyChanged("Lastname");
+            }
+        }
+
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
